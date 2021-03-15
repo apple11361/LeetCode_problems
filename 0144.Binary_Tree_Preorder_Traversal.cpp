11fc6ac3e4dd9d5aad6738b8b5d78ac6b7dd5588
Junde Li (李俊德) <Junde.Li@mediatek.com>
2021-03-15 15:32:00 +0800

    Solve problem 144
    As title.

---
+++ 0144.Binary_Tree_Preorder_Traversal.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(): val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x): val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right): val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    vector<int> preorderTraversal(TreeNode* root)
+    {
+        dfs(root);
+
+        return answer;
+    }
+private:
+    void dfs(TreeNode *node)
+    {
+        if(!node)
+            return;
+        
+        answer.push_back(node->val);
+        dfs(node->left);
+        dfs(node->right);
+    }
+
+    vector<int> answer;
+};
+
+int main(int argc, char *argv[])
+{
+    TreeNode root[3];
+    Solution sol;
+    vector<int> answer;
+
+    root[0].val = 1;
+    root[0].left = NULL;
+    root[0].right = &root[1];
+    root[1].val = 2;
+    root[1].left = &root[2];
+    root[1].right = NULL;
+    root[2].val = 3;
+    root[2].left = NULL;
+    root[2].right = NULL;
+
+    answer = sol.preorderTraversal(root);
+
+    for(int i = 0; i < answer.size(); ++i)
+    {
+        printf("%d ", answer[i]);
+    }
+
+    return 0;
+}
